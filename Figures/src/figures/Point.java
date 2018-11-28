78ddecc5f4639248cd1a17cfdee106dc6c522b6b
Hyqooo <andreykostenko99@gmail.com>
2018-11-28 16:25:10 +0300

    Removed setters - Point class

--- Figures/src/figures/Point.java
+++ Figures/src/figures/Point.java
@@ -10,14 +10,6 @@ public class Point {
         this.y = y;
     }
     
-    public void setX(int x){
-        this.x = x;
-    }
-    
-    public void setY(int y){
-        this.y = y;
-    }
-    
     @Override
     public String toString(){
         return x + "," + " " + y;
