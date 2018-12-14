68a6168ad0db3d4db2badebdad0dd03bf247d2c2
Hyqooo <andreykostenko99@gmail.com>
2018-12-14 09:44:48 +0300

    Larina Test 2

--- TestWork2Larina/src/testwork2larina/Telegram.java
+++ TestWork2Larina/src/testwork2larina/Telegram.java
@@ -10,6 +10,20 @@ public class Telegram {
             System.out.println("Tochka");
         
         this.text = new StringBuilder(text);
+        parseToTelegram();
+    }
+    
+    private void parseToTelegram(){
+        for (int i = 0; i < text.length(); i++){
+            if (text.charAt(i) == ','){
+                text.setCharAt(i, ' ');
+                text.insert(i + 1, "зпт ");
+            }
+            if (text.charAt(i) == '.'){
+                text.setCharAt(i, ' ');
+                text.insert(i + 1, "тчк");
+            }
+        }
     }
     
     @Override
