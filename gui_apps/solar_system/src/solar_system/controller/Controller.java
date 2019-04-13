a7c5966e62c13a8f2d27d5bc15cdd9d1056f0fe7
Hyqooo <andreykostenko99@gmail.com>
2019-04-13 18:45:11 +0300

    added gui

--- gui_apps/solar_system/src/solar_system/controller/Controller.java
+++ gui_apps/solar_system/src/solar_system/controller/Controller.java
@@ -11,6 +11,7 @@ import javafx.stage.Stage;
 import solar_system.view.View;
 
 public class Controller {
+
     private static ArrayList<SkyBody> bodies;
 
     // earth radius / earth orbit radius
@@ -18,11 +19,16 @@ public class Controller {
     private static double SCALE_OF_THE_ORBIT = 1;
     private static double TIME_SCALE = 0.0002;
 
+    private static boolean showOrbits = true;
+    private static boolean showPlanets = true;
+    private static boolean showSatellites = true;
+    private static boolean showNames = true;
+
     static View view;
 
     public static void planetInitialize(Stage primaryStage) {
         Position zeroPosition = new Position(0, 0);
-        
+
         SkyBody sun = new SkyBody(new Position(0, 0), 0, EARTH_RADIUS_COEFF * 15, 0, null, "Sun");
         SkyBody mercury = new SkyBody(zeroPosition, SCALE_OF_THE_ORBIT * 0.3871, EARTH_RADIUS_COEFF * 0.5, 88, null, "Mercury");
         SkyBody venus = new SkyBody(zeroPosition, SCALE_OF_THE_ORBIT * 0.7233, EARTH_RADIUS_COEFF * 0.7, 225, null, "Venus");
@@ -32,7 +38,7 @@ public class Controller {
         ArrayList<SkyBody> earth_satellites = new ArrayList<>();
         earth_satellites.add(moon);
         SkyBody earth = new SkyBody(zeroPosition, SCALE_OF_THE_ORBIT, EARTH_RADIUS_COEFF, 365, earth_satellites, "Earth");
-        
+
         // Mars
         SkyBody phobos = new SkyBody(new Position(0, 0), SCALE_OF_THE_ORBIT * 0.04, EARTH_RADIUS_COEFF * 0.1, 0.318, null, "Phobos");
         SkyBody deimos = new SkyBody(new Position(0, 0), SCALE_OF_THE_ORBIT * 0.07, EARTH_RADIUS_COEFF * 0.1, 1.263, null, "Deimos");
@@ -40,21 +46,21 @@ public class Controller {
         mars_satellites.add(phobos);
         mars_satellites.add(deimos);
         SkyBody mars = new SkyBody(zeroPosition, SCALE_OF_THE_ORBIT * 1.5237, EARTH_RADIUS_COEFF * 0.7, 687, mars_satellites, "Mars");
-        
+
         bodies = new ArrayList<>();
         bodies.add(sun);
         bodies.add(mercury);
         bodies.add(venus);
         bodies.add(earth);
         bodies.add(mars);
-        
+
         // Set scene
         view = new View(primaryStage);
 
         Timeline timeline = new Timeline();
         timeline.setAutoReverse(true);
         timeline.setCycleCount(Timeline.INDEFINITE);
-        
+
         // canvas update
         AnimationTimer timer = new AnimationTimer() {
             @Override
@@ -62,32 +68,52 @@ public class Controller {
                 display();
             }
         };
-        
+
         timer.start();
         timeline.play();
     }
-    
-    public static void display(){
+
+    public static void display() {
         long time = System.currentTimeMillis();
-        
+
         view.clearCanvas();
         PositionEvaluator.setScale(SCALE_OF_THE_ORBIT);
-        for (SkyBody body : bodies){
+        for (SkyBody body : bodies) {
             long offset = System.currentTimeMillis();
-            view.redrawOrbit(body, SCALE_OF_THE_ORBIT);
+            view.redrawOrbit(body, SCALE_OF_THE_ORBIT, showOrbits);
             // WARNING: SCALE_OF_THE_ORBIT applied to the size of the planets
-            view.redrawPlanet(body, SCALE_OF_THE_ORBIT, PositionEvaluator.evaluatePosition(body, TIME_SCALE, 2 * offset - time));
-            view.redrawName(body, PositionEvaluator.evaluatePosition(body, TIME_SCALE, 2 * offset - time), SCALE_OF_THE_ORBIT);
-            if (body.getSatellites() != null){
-                for (SkyBody sat : body.getSatellites()){
+            view.redrawPlanet(body, SCALE_OF_THE_ORBIT, PositionEvaluator.evaluatePosition(body, TIME_SCALE, 2 * offset - time), showPlanets);
+            view.redrawName(body, PositionEvaluator.evaluatePosition(body, TIME_SCALE, 2 * offset - time), SCALE_OF_THE_ORBIT, showNames);
+            if (body.getSatellites() != null) {
+                for (SkyBody sat : body.getSatellites()) {
                     Position bodyPos = PositionEvaluator.evaluatePosition(body, TIME_SCALE, 2 * offset - time);
                     sat.setAnchorPoint(bodyPos.x(), bodyPos.y());
-                    view.redrawOrbit(sat, SCALE_OF_THE_ORBIT);
+                    view.redrawOrbit(sat, SCALE_OF_THE_ORBIT, showOrbits);
                     // WARNING: SCALE_OF_THE_ORBIT applied to the size of the planets
-                    view.redrawPlanet(sat, SCALE_OF_THE_ORBIT, PositionEvaluator.evaluatePosition(sat, TIME_SCALE, 2 * offset - time));
-                    view.redrawName(sat, PositionEvaluator.evaluatePosition(sat, TIME_SCALE, 2 * offset - time), SCALE_OF_THE_ORBIT);
+                    view.redrawPlanet(sat, SCALE_OF_THE_ORBIT, PositionEvaluator.evaluatePosition(sat, TIME_SCALE, 2 * offset - time), showSatellites);
+                    view.redrawName(sat, PositionEvaluator.evaluatePosition(sat, TIME_SCALE, 2 * offset - time), SCALE_OF_THE_ORBIT, showNames);
                 }
             }
         }
+    }
+
+    public static void changeShowOrbits(boolean value) {
+        showOrbits = value;
+    }
+
+    public static void changeShowPlanets(boolean value) {
+        showPlanets = value;
+    }
+
+    public static void changeShowSatellites(boolean value) {
+        showSatellites = value;
+    }
+
+    public static void changeShowNames(boolean value) {
+        showNames = value;
+    }
+    
+    public static void changeTimeScale(Number value){
+        TIME_SCALE = value.doubleValue() * 0.002;
     }
 }
--- gui_apps/solar_system/src/solar_system/view/View.java
+++ gui_apps/solar_system/src/solar_system/view/View.java
@@ -1,17 +1,23 @@
 package solar_system.view;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventType;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import solar_system.controller.Controller;
 import solar_system.model.Position;
 import solar_system.model.SkyBody;
 
@@ -33,14 +39,61 @@ public class View {
         Label header = new Label("Solar system control");
         header.setFont(new Font(20));
 
-        Button btn = new Button("Test");
-        Button btn2 = new Button("Test2");
+        // checkboxes
+        CheckBox showOrbits = new CheckBox("Orbits");
+        showOrbits.setSelected(true);
+        CheckBox showPlanets = new CheckBox("Planets");
+        showPlanets.setSelected(true);
+        CheckBox showSatellites = new CheckBox("Satellites");
+        showSatellites.setSelected(true);
+        CheckBox showNames = new CheckBox("Names");
+        showNames.setSelected(true);
+        Button niceSpeed = new Button("Nice speed");
+       
+
+        showOrbits.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            public void changed(ObservableValue<? extends Boolean> ov,
+                    Boolean old_val, Boolean new_val) {
+                Controller.changeShowOrbits(new_val);
+            }
+        });
+
+        showPlanets.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            public void changed(ObservableValue<? extends Boolean> ov,
+                    Boolean old_val, Boolean new_val) {
+                Controller.changeShowPlanets(new_val);
+            }
+        });
+
+        showSatellites.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            public void changed(ObservableValue<? extends Boolean> ov,
+                    Boolean old_val, Boolean new_val) {
+                Controller.changeShowSatellites(new_val);
+            }
+        });
+
+        showNames.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            public void changed(ObservableValue<? extends Boolean> ov,
+                    Boolean old_val, Boolean new_val) {
+                Controller.changeShowNames(new_val);
+            }
+        });
+
+        // time scale
+        Slider timeSlider = setSlider();
+        timeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                Controller.changeTimeScale(newValue);
+            }
+        });
 
         // control
         VBox right = new VBox(10);
         right.setAlignment(Pos.TOP_RIGHT);
         right.setPadding(new Insets(5));
-        right.getChildren().addAll(header, btn, btn2);
+        right.getChildren().addAll(header, showOrbits, showPlanets, showSatellites,
+                showNames, timeSlider, niceSpeed);
 
         HBox holder = new HBox(5);
         holder.setPadding(new Insets(5));
@@ -58,12 +111,26 @@ public class View {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-    
-    public void clearCanvas(){
+
+    private Slider setSlider() {
+        Slider slider = new Slider(0, 2, 1);
+        slider.setShowTickLabels(true);
+        slider.setShowTickMarks(true);
+        slider.setMajorTickUnit(0.1);
+        slider.setMinorTickCount(1);
+        slider.setBlockIncrement(0.1);
+        return slider;
+    }
+
+    public void clearCanvas() {
         gc.clearRect(0, 0, 900, 700);
     }
 
-    public void redrawOrbit(SkyBody body, double scale) {
+    public void redrawOrbit(SkyBody body, double scale, boolean show) {
+        if (!show) {
+            return;
+        }
+
         double radius = body.getOrbitRadius() * 400 * scale;
         System.out.println(radius);
         gc.strokeOval(
@@ -73,24 +140,31 @@ public class View {
                 radius, radius);
     }
 
-    public void redrawPlanet(SkyBody body, double scale, Position pos) {
+    public void redrawPlanet(SkyBody body, double scale, Position pos, boolean show) {
+        if (!show) {
+            return;
+        }
+
         double radius = body.getRadius() * scale;
         System.out.println("Planet redraw - " + body.getName() + ":" + pos);
         double redraw_position_x = pos.x() + canvas.getWidth() / 2 - radius;
         double redraw_position_y = pos.y() + canvas.getHeight() / 2 - radius;
-        
+
         System.out.println("Actual planet coords: " + "\nx: " + redraw_position_x + "\ny: " + redraw_position_y);
-                
+
         gc.strokeOval(redraw_position_x, redraw_position_y, radius * 2, radius * 2);
     }
-    
-    public void redrawName(SkyBody body, Position pos, double scale){
+
+    public void redrawName(SkyBody body, Position pos, double scale, boolean show) {
+        if (!show) {
+            return;
+        }
+
         double radius = body.getRadius() * scale;
         System.out.println("name redraw - " + body.getName() + ": " + pos);
         double redraw_position_x = pos.x() + canvas.getWidth() / 2 - radius;
         double redraw_position_y = pos.y() + canvas.getHeight() / 2 - radius;
-        
-        
+
         gc.strokeText(body.getName(), redraw_position_x + radius * 2, redraw_position_y + radius * 2);
     }
 }
